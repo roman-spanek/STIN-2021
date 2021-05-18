795513bdb2f0cf0d0cde6152c98410d03091d96f
Roman_Spanek <roman.spanek@tul.cz>
2021-05-18 21:08:11 +0200

    Updated Unit test public removed

--- src/test/java/cz/tul/roman/spanek/stin/StinToTestImplTest.java
+++ src/test/java/cz/tul/roman/spanek/stin/StinToTestImplTest.java
@@ -5,17 +5,18 @@ import cz.tul.roman.spanek.stin.impl.StinToTestImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class StinToTestImplTest {
+class StinToTestImplTest {
 
     private StinToTestImpl stinToTest = new StinToTestImpl();;
 
     @Test
-    public void sumMehodTest() {
-        Assertions.assertEquals(stinToTest.sum(10 , 2), 12);
+    void sumMehodTest() {
+        Assertions.assertEquals(12, stinToTest.sum(10 , 2));
     }
 
     @Test
     void sumMehodTest2(){
-        Assertions.assertEquals(stinToTest.sum(10 , 2), 12);
+        Assertions.assertEquals(12, stinToTest.sum(10 , 2));
     }
+
 }
