91407c4feb3a0146fa8eedbe9972bf2495ac97c2
Roman_Spanek <roman.spanek@tul.cz>
2021-05-18 12:13:25 +0200

    junit testing 2

--- src/test/java/cz/tul/roman/spanek/stin/StinToTestImplTest.java
+++ src/test/java/cz/tul/roman/spanek/stin/StinToTestImplTest.java
@@ -1,19 +1,20 @@
 package cz.tul.roman.spanek.stin;
 
 import cz.tul.roman.spanek.stin.impl.StinToTestImpl;
-import org.junit.Assert;
-import org.junit.Before;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class StinToTestImplTest {
-    private StinToTestImpl stinToTest;
-    @Before
-    public void init(){
-        stinToTest = new StinToTestImpl();
-    }
+    private StinToTestImpl stinToTest = new StinToTestImpl();;
 
     @Test
     public void sumMehodTest() {
-        Assert.assertEquals(stinToTest.sum(10 , 2), 12);
+        Assertions.assertEquals(stinToTest.sum(10 , 2), 12);
+    }
+
+    @Test
+    public void sumMehodTest2(){
+        Assertions.assertEquals(stinToTest.sum(10 , 3), 12);
     }
 }
