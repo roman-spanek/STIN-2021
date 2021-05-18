33370124f74d6ef2b8bc64583d01f9e72aaeaf4b
Roman_Spanek <roman.spanek@tul.cz>
2021-05-18 18:46:01 +0200

    junit testing 5

--- src/main/java/cz/tul/roman/spanek/stin/impl/StinToTestImpl.java
+++ src/main/java/cz/tul/roman/spanek/stin/impl/StinToTestImpl.java
@@ -5,7 +5,7 @@ import cz.tul.roman.spanek.stin.interfaces.IStinToTest;
 public class StinToTestImpl implements IStinToTest {
     @Override
     public int sum(int a, int b) {
-        a = a +b;
+        a = a +b + 10;
         return a + b +1;
     }
 }
