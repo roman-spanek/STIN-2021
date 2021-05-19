7196601d7bf10492785121de9517e1a4addcddc7
Roman_Spanek <roman.spanek@tul.cz>
2021-05-19 11:18:16 +0200

    STIN-2021 add SquareImpl + unit test

---
+++ src/test/java/cz/tul/roman/spanek/stin/SquareImplTest.java
@@ -0,0 +1,15 @@
+package cz.tul.roman.spanek.stin;
+
+import cz.tul.roman.spanek.stin.impl.SquareImpl;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class SquareImplTest {
+
+    private SquareImpl square = new SquareImpl();
+
+    @Test
+    void testSquare(){
+        Assert.assertEquals(4, square.square(2));
+    }
+}
